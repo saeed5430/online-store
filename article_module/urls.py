9713423557c56b1ff6b5b96e6b7e48271a82ac4d
saeed5430 <abdollahi003@gmail.com>
2025-04-09 15:09:01 +0330

    new_change

--- article_module/urls.py
+++ article_module/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.ArticleListView.as_view(),name='articles_list')
+    path('',views.ArticleListView.as_view(),name='articles_list'),
+    path('<str:category>',views.ArticleListView.as_view(),name='articles_list_component')
 ]
--- article_module/views.py
+++ article_module/views.py
@@ -4,7 +4,7 @@ from django.views.generic import ListView
 from django.shortcuts import render
 from django.views import View
 
-from article_module.models import Article
+from article_module.models import Article, ArticleCategory
 
 
 class ArticleView(View):
@@ -15,10 +15,23 @@ class ArticleView(View):
 
 class ArticleListView(ListView):
     model=Article
-    paginate_by = 5
+    paginate_by = 1
     template_name = 'article_module/article_page.html'
 
     def get_context_data(self,*args, **kwargs):
         context = super(ArticleListView,self).get_context_data(*args,**kwargs)
         context['date'] = datetime2jalali(self.request.user.date_joined)
         return context
+
+    def get_queryset(self):
+        query = super(ArticleListView, self).get_queryset()
+        query = query.filter(is_active=True)
+        category_name = self.kwargs.get('category')
+        if category_name:
+            query = query.filter(selected_category__url_title__iexact=category_name)
+        return query
+
+
+def article_categories_component(request):
+    main_categories=ArticleCategory.objects.filter(is_active=True)
+    return render(request,"article_module/components/article_categories_component.html",context={"main_categories":main_categories})
